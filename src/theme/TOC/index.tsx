62024118fc68a431a59239c675a40431b5703ec0
mxsm <ljbmxsm@gmail.com>
2022-11-08 23:09:46 +0800

    remove ad from contents

--- src/theme/TOC/index.tsx
+++ src/theme/TOC/index.tsx
@@ -24,29 +24,7 @@ export default function TOC({ toc, editUrl, ...props }) {
       <OriginalTOC toc={toc} {...props} />
       <EditThisPage editUrl={editUrl} />
 
-      {activeAd && (
-        <div className="internal-ad">
-          <a
-            href={activeAd.ad_url.url}
-            target={activeAd.ad_url.target}
-            // onClick={e => trackClick(activeAd.ad_id, e)}
-          >
-            {/* Reponsive image since Prismic supports it out of the box */}
-            <picture>
-              <source media="(min-width: 37.5em)" src={activeAd.ad_image.url} />
-              <source src={activeAd.ad_image['1x'].url} />
-              <img
-                src={activeAd.ad_image.url}
-                alt={activeAd.ad_image.alt}
-                height={activeAd.ad_image['1x'].dimensions.height}
-                width={activeAd.ad_image['1x'].dimensions.width}
-              />
-              <p>{activeAd.ad_image.alt}</p>
-            </picture>
-            <PrismicRichText field={activeAd.ad_copy} />
-          </a>
-        </div>
-      )}
+
     </div>
   );
 }
