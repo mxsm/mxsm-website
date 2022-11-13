4c2ace19ea802a4da299e32eba804daf0a3d58cf
mxsm <ljbmxsm@gmail.com>
2022-11-13 11:47:42 +0800

    Delete src directory

--- src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-// Functions
-export { sendRequest, RequestMethod } from "./requestUtils";
-export { openWindow, isMobile, setClipBoardText } from "./funcUtils";
-export { switchColor } from "./styleUtils";
-
-// Constants
-export { themeColor } from "./styleConstants";
